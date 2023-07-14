e5f251337cf1586d08187afb6bddb5005608beff
Rick Brannan <43759793+RickBrannan@users.noreply.github.com>
2023-07-14 16:06:05 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# apostolic-fathers
+ Spot for Rick to put stuff when he's messing with apostolic fathers data
---
+++ code/python/apostolic-fathers/count_tokens.py
@@ -0,0 +1,49 @@
+import os
+import re
+
+# find book word counts for both NT and Apostolic Fathers
+# in order to compare
+
+# MorphGNT SBLGNT
+morphgnt_dir = "c:/git/MorphGNT/sblgnt/"
+
+# Apostolic Fathers
+apostolic_fathers_dir = "c:/git/jtauber/apostolic-fathers/texts/"
+
+# output dir
+output_dir = "c:/git/RickBrannan/aposotlic-fathers/data/tsv/"
+
+book_word_counts = {}
+
+for filename in os.listdir(morphgnt_dir):
+    if filename.endswith(".txt"):
+        print("Processing " + filename)
+        abbrev = re.sub(r'\.txt$', '', filename).split('-')[1]
+        # open the file as a list of lines
+        # since it is one line per word, this gives word count.
+        with open(morphgnt_dir + filename, encoding="utf8") as f:
+            lines = f.readlines()
+            book_word_counts[abbrev] = len(lines)
+
+for filename in os.listdir(apostolic_fathers_dir):
+    if filename.endswith(".txt"):
+        print("Processing " + filename)
+        # remove first four chars and .txt extension
+        abbrev = re.sub(r'^\d{3}-', '', filename).split('.')[0]
+        # open the file as a list of lines
+        book_word_count = 0
+        with open(apostolic_fathers_dir + filename, encoding="utf8") as f:
+            for line in f.readlines():
+                # remove leading and trailing whitespace
+                line = line.strip()
+                # remove punctuation
+                line = re.sub(r'^[\d.]+ ', '', line)
+                # split on spaces to get word count
+                book_word_count += len(line.split(' '))
+            book_word_counts[abbrev] = book_word_count
+
+# report book_word_counts
+with open("book_word_counts.txt", "w") as f:
+    for book in book_word_counts:
+        print(book + "\t " + str(book_word_counts[book]))
+        f.write(book + "\t " + str(book_word_counts[book]) + "\n")
